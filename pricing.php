0f70d3a3e6b7879c8bc317b171034855e7103ee7
queenierose <qrccorona@gmail.com>
2016-11-04 16:09:21 +0800

    Add files via upload

---
+++ pricing.php
@@ -0,0 +1,425 @@
+<div class="container single-header" style="margin-top: 10px; margin-bottom: 10px; padding-top: 10px; padding-bottom: 10px; background-color: ">
+<div class="row ">
+<div class="col-md-12"><h1 class="clear" style="text-align: center; color: #464646; font-size: 40px; line-height: 40px; font-weight: 300;">Choose a Website Plan</center></div>
+</div>
+</div>
+ 
+ 
+<div id="table-content" class="container">
+<div class="row">
+<div class="col-md-12">
+<div class="tab-slider-wrapper">
+<dl class="display-table">
+<dt class="green-text">Finance Your Website</dt>
+<dd>
+<div class="switcher">
+<a href="javascript:;"></a>
+</div>
+</dd>
+<dt>Website Packages</dt>
+</dl>
+</div>
+<h3 class="desc">12 months to pay. Secure reliable hosting.</h3>
+<h3 class="desc desc2">One-time payment. Optional Hosting.</h3>
+<table id="choose-website-plan-table" cellspacing="4" cellpadding="0" width="100%">
+<thead class="finance-your-website-header">
+<th><h2>What’s Included</h2></th>
+<th>
+<h3>D-I-Y</h3>
+<span><strong>$29</strong>/mo</span>
+<p>Do-it-yourself option with hosting</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PLUS</h3>
+<span><strong>$99</strong>/mo</span>
+<p>Start small with a basic website</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PRO</h3>
+<span><strong>$159</strong>/mo</span>
+<p>Our most popular plan</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PREMIUM</h3>
+<span><strong>$249</strong>/mo</span>
+<p>Tailor fit design to get more sales</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th class="finance-last-item web-packages-last-item">
+<script type="text/javascript" src="https://august99.formstack.com/forms/js.php/a99home_copy"></script><noscript><a href="https://august99.formstack.com/forms/a99home_copy" title="Online Form">Online Form - August99 - Choose a Website Plan</a></noscript>
+</th>
+</thead>
+<thead class="website-packages-header">
+<th><h2>What’s Included</h2></th>
+<th>
+<h3>D-I-Y</h3>
+<span><strong>$200</strong></span>
+<p>Do-it-yourself option with hosting</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PLUS</h3>
+<span><strong>$999</strong></span>
+<p>Start small with a basic website</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PRO</h3>
+<span><strong>$1,799</strong></span>
+<p>Our most popular plan</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th>
+<h3>PREMIUM</h3>
+<span><strong>$2,999</strong></span>
+<p>Custom design for business</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</th>
+<th class="finance-last-item">
+<script type="text/javascript" src="https://august99.formstack.com/forms/js.php/a99home_copy"></script><noscript><a href="https://august99.formstack.com/forms/a99home_copy" title="Online Form">Online Form - August99 - Choose a Website Plan</a></noscript>
+</th>
+</thead>
+<tbody>
+<tr>
+<td align="left">Wordpress Theme</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p>Personalized</p>
+</td>
+<td><p>Personalized</p>
+</td>
+<td><p>Custom</p>
+</td>
+</tr>
+<tr>
+<td align="left">Secure Hosting</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+</tr>
+<tr>
+<td align="left">Mobile Responsive</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+</tr>
+<tr>
+<td align="left">Blog Section</td>
+<td></td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+</tr>
+<tr>
+<td align="left">Inner Page Creation</td>
+<td></td>
+<td><p>5 Pages</p>
+</td>
+<td><p>5 Pages</p>
+</td>
+<td><p>10 Pages</p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+</tr>
+<tr>
+<td align="left">Page Customization</td>
+<td></td>
+<td></td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+</tr>
+<tr>
+<td align="left">Shopping Cart</td>
+<td></td>
+<td></td>
+<td></td>
+<td><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</td>
+<td><p>Custom</p>
+</td>
+</tr>
+</tbody>
+</table>
+<div class="choose-website-plan-table-mobile">
+<div class="table-header mobile-finance">
+<dl class="display-table">
+<dt><h4>D-I-Y</h4></dt>
+<dd><span><strong>$29</strong>/mo</span></dd>
+</dl>
+<p>Do-it-yourself option with hosting</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-header mobile-package">
+<dl class="display-table">
+<dt><h4>D-I-Y</h4></dt>
+<dd><span><strong>$200</strong></span></dd>
+</dl>
+<p>Do-it-yourself option with hosting</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-body">
+<dl class="display-table">
+<dt>Wordpress Theme</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Secure Hosting</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Mobile Responsive</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Blog Section</dt>
+<dd></dd>
+</dl>
+<dl class="display-table">
+<dt>Inner Page Creation</dt>
+<dd></dd>
+</dl>
+<dl class="display-table">
+<dt>Page Customization</dt>
+<dd></dd>
+</dl>
+<dl class="display-table">
+<dt>Shopping Cart</dt>
+<dd></dd>
+</dl>
+</div>
+<div class="table-header mobile-finance">
+<dl class="display-table">
+<dt><h4>PLUS</h4></dt>
+<dd><span><strong>$99</strong>/mo</span></dd>
+</dl>
+<p>Start small with a basic website</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-header mobile-package">
+<dl class="display-table">
+<dt><h4>PLUS</h4></dt>
+<dd><span><strong>$999</strong></span></dd>
+</dl>
+<p>Start small with a basic website</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-body">
+<dl class="display-table">
+<dt>Wordpress Theme</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Secure Hosting</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Mobile Responsive</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Blog Section</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Inner Page Creation</dt>
+<dd><p>5 Pages</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Page Customization</dt>
+<dd></dd>
+</dl>
+<dl class="display-table">
+<dt>Shopping Cart</dt>
+<dd></dd>
+</dl>
+</div>
+<div class="table-header mobile-finance" style="background-color: #e5f5cc">
+<dl class="display-table">
+<dt><h4>PRO</h4></dt>
+<dd><span><strong>$159</strong>/mo</span></dd>
+</dl>
+<p>Our most popular plan</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-header mobile-package" style="background-color: #f5e6dc">
+<dl class="display-table">
+<dt><h4>PRO</h4></dt>
+<dd><span><strong>$1,799</strong></span></dd>
+</dl>
+<p>Our most popular plan</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-body">
+<dl class="display-table">
+<dt>Wordpress Theme</dt>
+<dd><p>Personalized</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Secure Hosting</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Mobile Responsive</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Blog Section</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Inner Page Creation</dt>
+<dd><p>5 Pages</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Page Customization</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Shopping Cart</dt>
+<dd></dd>
+</dl>
+</div>
+<div class="table-header mobile-finance">
+<dl class="display-table">
+<dt><h4>PREMIUM</h4></dt>
+<dd><span><strong>$249</strong>/mo</span></dd>
+</dl>
+<p>Tailor fit design to get more sales</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-header mobile-package">
+<dl class="display-table">
+<dt><h4>PREMIUM</h4></dt>
+<dd><span><strong>$2,999</strong></span></dd>
+</dl>
+<p>Custom design for business</p>
+<a data-toggle="modal" data-target="#myModal" href="javascript:;" class="plain-btn"><span class="mid-content display-inline" title="Get Started">Get Started</span></a>
+</div>
+<div class="table-body">
+<dl class="display-table">
+<dt>Wordpress Theme</dt>
+<dd><p>Personalized</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Secure Hosting</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Mobile Responsive</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Blog Section</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Inner Page Creation</dt>
+<dd><p>10 Pages</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Page Customization</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Shopping Cart</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+</div>
+<div class="table-header mobile-finance finance-last-item" style="background-color: #e5f5cc">
+<script type="text/javascript" src="https://august99.formstack.com/forms/js.php/a99home_copy"></script><noscript><a href="https://august99.formstack.com/forms/a99home_copy" title="Online Form">Online Form - August99 - Choose a Website Plan</a></noscript>
+</div>
+<div class="table-header mobile-package finance-last-item" style="background-color: #f5e6dc">
+<script type="text/javascript" src="https://august99.formstack.com/forms/js.php/a99home_copy"></script><noscript><a href="https://august99.formstack.com/forms/a99home_copy" title="Online Form">Online Form - August99 - Choose a Website Plan</a></noscript>
+</div>
+<div class="table-body">
+<dl class="display-table">
+<dt>Wordpress Theme</dt>
+<dd><p>Custom</p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Secure Hosting</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Mobile Responsive</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Blog Section</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Inner Page Creation</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Page Customization</dt>
+<dd><p><img src="https://www.august99.com/wp-content/uploads/2016/10/check-ico.png"/></p>
+</dd>
+</dl>
+<dl class="display-table">
+<dt>Shopping Cart</dt>
+<dd><p>Custom</p>
+</dd>
+</dl>
+</div>
+</div>
+<i></i>
+</div>
+</div>
+</div>
